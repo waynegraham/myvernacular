a32238ab0128e97a33613d073d556c166ff5dc2f
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-12 16:58:04 +0000

    refs for the gems used

--- trunk/web/lib/flickr.rb
+++ trunk/web/lib/flickr.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
+
+# http://github.com/jnunemaker/httparty/tree/master
 require 'httparty'
+
+# http://github.com/tenderlove/nokogiri/tree/master
 require 'nokogiri'
 
 class Flickr
@@ -9,18 +13,22 @@ class Flickr
     include HTTParty
     
     format :xml
+    
     base_uri 'http://api.flickr.com/services/rest'
     
     class << self
       
+      # http://www.flickr.com/services/api/flickr.people.getPublicPhotos.html
       def user_photos(userid)
         get '', :query=>{:method => 'flickr.people.getPublicPhotos', :user_id => userid}
       end
       
-      def photo(photo_id)
-        get '', :query=>{:method => 'flickr.people.getPublicPhotos', :photo_id => photo_id}
+      # http://www.flickr.com/services/api/flickr.photos.getSizes.html
+      def photo_sizes(photo_id)
+        get '', :query=>{:method => 'flickr.photos.getSizes', :photo_id => photo_id}
       end
       
+      # http://www.flickr.com/services/api/flickr.photos.getWithGeoData.html
       def photo_location(photo_id)
         get '', :query=>{:method => 'flickr.photos.geo.getLocation', :photo_id => photo_id}
       end
