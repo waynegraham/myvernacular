279ffd038483e8cd6976cdce86c41bb74bf49b0e
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-12 19:33:47 +0000

    Basic Building validation tests

--- web/test/unit/building_test.rb
+++ web/test/unit/building_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class BuildingTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "validations" do
+    b = Building.create
+    assert_equal false, b.valid?
+    errors = b.errors
+    assert_equal 2, errors.size
+    assert_equal "can't be blank", errors['name']
+    assert_equal "can't be blank", errors['country']
   end
+  
 end
