1f1e9c665695f002f7f97961f5f18476c24f58ba
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-12 19:33:47 +0000

    Basic Building validation tests

--- trunk/web/app/models/building.rb
+++ trunk/web/app/models/building.rb
@@ -1,2 +1,10 @@
 class Building < ActiveRecord::Base
+  
+  belongs_to :user
+  
+  validates_presence_of :name
+  validates_presence_of :country
+  validates_numericality_of :lat, :allow_nil=>true
+  validates_numericality_of :lon, :allow_nil=>true
+  
 end
