38b1d60a0a582e52fe0bbbf66b9fc17935331af7
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-12 19:39:49 +0000

    Added resource_controller plugin Added resource_controller to buildings controller added stub, new view for buildings

--- trunk/web/app/controllers/users_controller.rb
+++ trunk/web/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  # should do resource_controller here!
+  
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
   
