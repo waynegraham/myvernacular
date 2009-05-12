3df93e95fcf83595358f7ef8635abc4b2db31843
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-12 19:28:49 +0000

    Basic Building resource added, with index action and view

--- trunk/web/config/routes.rb
+++ trunk/web/config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :buildings
   
   map.resource :account, :controller => "users"
   
@@ -6,7 +7,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resource :user_session
   
-  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  map.root :controller => "buildings", :action => "index"
   
   # The priority is based upon order of creation: first created -> highest priority.
 
