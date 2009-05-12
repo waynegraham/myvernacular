23aa21bfb1a92fe8018ee4e2041fd9ba208e66f9
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-12 18:37:20 +0000

    Removed the sample css from app.css Added css for forms, based off of yui-grids Method added to UsersHelper for dynamically rendering a users fields

--- trunk/web/app/helpers/users_helper.rb
+++ trunk/web/app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  
+  # for dynamically rendering the "account" view fields
+  def user_content_fields
+    %W(
+    login
+    flickr_username
+    first_name
+    last_name
+    email
+    bio
+    )
+  end
+  
 end
