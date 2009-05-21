5f9cfb893b88bc4c497cfd88c144a93b86badc53
goodieBoy <goodieBoy@5c145826-3ef2-11de-ba0d-4f26e5d835f1>
2009-05-21 15:21:04 +0000

    Created helpers for top and bottom layout elements (css/script) Changed buildings/show to use these new helpers

--- trunk/web/app/helpers/application_helper.rb
+++ trunk/web/app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  def page_end
-    @page_end ||= []
+  # stores items for the layout to render at the top of the page (in the head tag)
+  # ...for css link tags
+  def html_top_items
+    @html_top_items ||= []
+  end
+  
+  # stores items for the layout to render at the bottom of the page (after the html tag)
+  # ...for script tags
+  def html_bottom_items
+    @html_bottom_items ||= []
   end
   
 end
